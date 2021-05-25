b5d72585d14b0dd9af375267813735c5f02558f2
wwn_25 <83991017+juwan-25@users.noreply.github.com>
2021-05-25 09:20:24 +0900

    switch 적용 후(보드게임과 관련 없음)

--- 07-02 switch.c
+++ 07-02 switch.c
@@ -6,6 +6,37 @@ void main(void) {
 	printf("숫자를 입력하세요(1~8까지) : ");
 	scanf("%d", &input);
 
+	switch (input)
+	{
+	case 1:
+		printf("피카츄\n");
+		break;
+	case 2:
+		printf("라이츄\n");
+		break;
+	case 3:
+		printf("꼬부기\n");
+		break;
+	case 4:
+		printf("버터풀\n");
+		break;
+	case 5:
+		printf("파이리\n");
+		break;
+	case 6:
+		printf("야도란\n");
+		break;
+	case 7:
+		printf("피죤투\n");
+		break;
+	case 8:
+		printf("또가스\n");
+		break;
+	default :
+		printf("잘못 입력하였습니다.\n");
+		break;
+	}
+
 	if (input == 1) 
 	printf("피카츄\n");
 	else if (input == 2)
@@ -15,7 +46,7 @@ void main(void) {
 	else if (input == 4)
 	printf("버터풀\n");
 	else if (input == 5)
-	printf("꼬부기\n");
+	printf("파이리\n");
 	else if (input == 6)
 	printf("야도란\n");
 	else if (input == 7)
@@ -23,5 +54,5 @@ void main(void) {
 	else if (input == 8)
 	printf("또가스\n");
 	else
-	printf("잘못 입력하였습니다.");
+	printf("잘못 입력하였습니다.\n");
 }
