5ed860bca29c5e42cf738ccd886bc7f1724a5c33
wwn_25 <83991017+juwan-25@users.noreply.github.com>
2021-06-15 10:01:05 +0900

    [문법 공부] 구구단에 continue 추가

--- 구구단.c
+++ 구구단.c
@@ -7,19 +7,20 @@ void main(void)
 
 	for (int dan = 2; dan <= 9; dan++)
 	{
-		if(dan == 4) //3단까지만 출력, qkRkx쪽 for문에 대한 break
+		if(dan == 4) //3단까지만 출력, 바깥 쪽 for문에 대한 continue, 4단만 스킵
 		{
-			break;
+			continue;
 
 		}
 		for (int su = 1; su <= 9; su++)
 		{
 			if (su==5)
 			{ 
-				break;  //안 쪽 for문에 대한 break
+				continue;  //안 쪽 for문에 대한 continue,5만 스킵
 			}
 			printf("%d X %d = %d \n", dan, su, dan * su);
 		}
+
 		printf(" \n");
 	}
 }
