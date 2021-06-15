edee8075e644baebcd368036a30d53468c6196c2
wwn_25 <83991017+juwan-25@users.noreply.github.com>
2021-06-15 09:38:17 +0900

    [문법 연습] 구구단(for문 연습)_6 완성

--- 구구단.c
+++ 구구단.c
@@ -5,7 +5,7 @@ void main(void)
 	/* 이중 for문 */
 	/* dan : 2개, su : 9개 => 18개 경우의 수 */
 
-	for (int dan = 2; dan <= 3; dan++)
+	for (int dan = 2; dan <= 9; dan++)
 	{
 		for (int su = 1; su <= 9; su++)
 		{
