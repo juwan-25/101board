efef8d6b04b63d77f58e302b95cf9dd428f64a21
wwn_25 <83991017+juwan-25@users.noreply.github.com>
2021-06-29 10:14:24 +0900

    재귀함수 완성

--- 문법연습/08 - 04 재귀함수(recursive, recursion).c
+++ 문법연습/08 - 04 재귀함수(recursive, recursion).c
@@ -4,11 +4,11 @@ int factorial(int num) // 매개변수 parameter
 {
 	if (num == 1)
 		return 1;
+	return num * factorial(num - 1); // 3! = 3 * 2!, 2!= 2 * 1!, 1! = 1
 }
-
 void main (void) 
 {
-	int val = 1;
+	int val = 3;
 	int a = factorial(val); // 인수 argument
 	printf("%d 팩토리얼은 %d \n", val, a);
 }
