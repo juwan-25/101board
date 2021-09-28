0087e24c895d4454a747ce7a2476d683c0387a8e
Lee Juwan <83991017+juwan-25@users.noreply.github.com>
2021-09-28 12:11:43 +0900

    지역변수 바꾸기(call by pointer)

--- 문법연습/11 - 05 지역변수 바꾸기.c
+++ 문법연습/11 - 05 지역변수 바꾸기.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
-void changeA(int num);
+void changeA(int* num);
 
 void main(void)
 {
 	int A = 3;
 	printf("바꾸기 전 %d\n", A);
 	/*
-	함수에 변수 A가 들어가는 값 3을 넘기는 것이지
-	변수 A 자체를 넘기는 것이 아니다.
+	함수의 argument로 A의 주소값을 넘긴다.
+	매개변수 num은 변수 A를 가리킨다.
 	*/
-	changeA(A);
+	changeA(&A);
 	printf("바꾸기 후 %d\n", A);
 }
-void changeA(int num)
+void changeA(int* num)
 {
-	num = 5;
+	// num이 가리키고 있는 변수(A) 안에 들어있는 값을 5로 바꾼다.
+	*num = 5; //*가 없으면 num이라는 변수의 값이 바뀐다.
 }
