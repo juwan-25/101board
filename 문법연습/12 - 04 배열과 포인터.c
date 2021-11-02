c605ae488e08b3d028597048333fbc6684c1f239
Lee Juwan <83991017+juwan-25@users.noreply.github.com>
2021-11-02 09:01:23 +0900

    포인터를 배열처럼

--- 문법연습/12 - 04 배열과 포인터.c
+++ 문법연습/12 - 04 배열과 포인터.c
@@ -6,8 +6,11 @@ void main(void)
 	int* ptr = arr;
 
 	printf("%d %d %d %d\n", arr[0], arr[1], arr[2], arr[3]);
+	printf("%d %d %d %d\n", *(arr + 0), *(arr + 1), *(arr + 2), *(arr + 3));
+	//포인터를 배열처럼 쓸 수 있다
+	//BUT 포인터는 배열이 아니다
 	printf("%d %d %d %d\n", ptr[0], ptr[1], ptr[2], ptr[3]);
-	printf("%d %d %d %d\n", *ptr, *(ptr + 1), *(ptr + 2), *(ptr + 3));
+	printf("%d %d %d %d\n", *(ptr+0), *(ptr + 1), *(ptr + 2), *(ptr + 3));
 
 	printf("%d ", sizeof(arr));
 	printf("%d ", sizeof(ptr));
